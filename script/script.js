778fbcc9c0eeba7d51aabb7e5d0824bc0e0bbaff
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-11-22 22:06:26 -0300

    Adicionado JS do contador

---
+++ script/script.js
@@ -0,0 +1,53 @@
+function criarDataLancamento() {
+  if (!localStorage.getItem("releaseDate")) {
+    const date = new Date();
+    var mesAtual = date.getMonth();
+    localStorage.setItem("releaseDate", date.setMonth(mesAtual + 1));
+  }
+}
+
+function renderDataLancamento(){
+  const releasePage = document.querySelector(".contador-cards > h3 span");
+  var releaseDate = new Date(parseInt(localStorage.getItem("releaseDate")));
+   const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  }
+  console.log(releaseDate);
+  releasePage.innerHTML = releaseDate.toLocaleDateString("en-GB",options);
+}
+
+function countdownRender(){
+  var parsedCountdown = contdownConverted()
+  const timeElements = document.querySelectorAll(".contador-cards-list p.font-1-xxl")
+  timeElements.forEach((value,index) =>{
+    value.innerHTML = parsedCountdown[index]
+  })
+
+}
+
+function contdownConverted(){
+  var releaseDate = new Date(parseInt(localStorage.getItem("releaseDate")));
+  var currentDate = new Date();
+  var timeLeft = releaseDate.getTime() - currentDate.getTime();
+  var rateConversion = [24 * 60 * 60 * 1000,60*60*1000,60*1000,1000]
+
+  var parsedCountdown = rateConversion.map((rate) =>{
+    let convertedValue = parseInt(timeLeft / rate)
+    timeLeft = timeLeft % rate
+    return convertedValue;
+  })
+  
+  return parsedCountdown;
+ }
+
+criarDataLancamento();
+renderDataLancamento();
+setInterval(countdownRender,1000)
+
+
+
+
+
+
